a9195f0e37f36be9e847377350bac851d2c34d53
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-11-01 22:00:54 +0545

    django project

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('view.urls', namespace='main')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # serve project-level static files during development
+    try:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    except Exception:
+        # fallback: no STATICFILES_DIRS configured
+        pass
